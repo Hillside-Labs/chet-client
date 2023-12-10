794328966896b6f31ab986b27a78fbd91f5d4c7c
Eric Larson <elarson@Erics-MacBook-Pro.local>
2023-12-09 22:47:04 -0600

    Remove DSN related code
    We're only using sqlite on the client.

--- models/db.go
+++ models/db.go
@@ -2,9 +2,6 @@ package models
 
 import (
 	"fmt"
-	"net/url"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/glebarez/sqlite"
@@ -42,15 +39,6 @@ func (lc LocalConfig) String() {
 	fmt.Printf("LocalConfig:%+v\n", lc)
 }
 
-type DSN struct {
-	Host     string
-	User     string
-	Password string
-	DBName   string
-	Port     int
-	SSLMode  string
-}
-
 func Connect(fn string) (*gorm.DB, error) {
 	db, err := gorm.Open(sqlite.Open(fn), &gorm.Config{})
 	if err != nil {
@@ -59,49 +47,4 @@ func Connect(fn string) (*gorm.DB, error) {
 	err = db.AutoMigrate(&Record{}, &LocalConfig{})
 
 	return db, err
-}
-
-func (dsn *DSN) String() string {
-	return fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%d sslmode=%s",
-		dsn.Host, dsn.User, dsn.Password, dsn.DBName, dsn.Port, dsn.SSLMode,
-	)
-}
-
-func NewDSN(dburi string) (*DSN, error) {
-	url, err := url.Parse(dburi)
-	if err != nil {
-		return nil, err
-	}
-
-	pw, _ := url.User.Password()
-	port := 5432
-	if url.Port() != "" {
-		port, err = strconv.Atoi(url.Port())
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	path := strings.Split(strings.TrimLeft(url.Path, "/"), "/")
-
-	if len(path) == 0 {
-		return nil, fmt.Errorf("missing db name: %s", url.Path)
-	}
-
-	dbname := path[0]
-
-	sslmode := url.Query().Get("sslmode")
-	if sslmode == "" {
-		sslmode = "disable"
-	}
-
-	return &DSN{
-		Host:     url.Hostname(),
-		User:     url.User.Username(),
-		Password: pw,
-		DBName:   dbname,
-		Port:     port,
-		SSLMode:  sslmode,
-	}, nil
 }
